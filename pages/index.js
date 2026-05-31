a641255a7e5f0599d4b0f24b1b11331917838a13
pratisig <37810753+pratisig@users.noreply.github.com>
2026-05-31 12:54:56 +0000

    feat: add pages/index.js and public/dashboard.html

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/dashboard.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="EBOLA-MONITOR v4.0"
+    />
+  );
+}
